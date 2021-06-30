c793b4754902e6b701595ab2ad924d43ebd33068
mllrdnl <mllr.dnl@gmail.com>
2021-06-30 20:04:44 +0000

    finished assignment

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandom = arr => {
+    var randomNumber = Math.floor(Math.random() * arr.length);
+    return randomNumber;
+  };
+  console.log(getRandom(who));
+  console.log(getRandom(action));
+  console.log(getRandom(what));
+  console.log(getRandom(when));
+
+  const generateExcuse = () => {
+    return (
+      who[getRandom(who)] +
+      " " +
+      action[getRandom(action)] +
+      " " +
+      what[getRandom(what)] +
+      " " +
+      when[getRandom(when)]
+    );
+  };
+
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
